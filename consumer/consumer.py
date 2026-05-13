3443153885742956e448d52a8634835d0b94fb9e
diya94 <diyakarmakar365@gmail.com>
2026-05-13 15:53:15 +0530

    removed spark, updated consumer to write to PostgreSQL

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,5 +1,15 @@
 from kafka import KafkaConsumer
 import json
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname="postgres",
+    user="postgres",
+    password="postgres123",
+    host="localhost",
+    port=5433
+)
+cur = conn.cursor()
 
 consumer = KafkaConsumer(
     'ecommerce_topic',
@@ -9,7 +19,17 @@ consumer = KafkaConsumer(
 )
 
 print("Consumer started...")
-
 for message in consumer:
     data = message.value
-    print(data)
+    cur.execute("""
+        INSERT INTO raw_data
+        (order_id, user_id, product_id, product_name,
+         category, price, quantity, event_type,
+         timestamp, payment_method, city)
+        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+        (data['order_id'], data['user_id'], data['product_id'],
+         data['product_name'], data['category'], data['price'],
+         data['quantity'], data['event_type'], data['timestamp'],
+         data['payment_method'], data['city']))
+    conn.commit()
+    print(f"Inserted order {data['order_id']}")
